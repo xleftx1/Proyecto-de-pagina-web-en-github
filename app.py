93c741dc67e6d69e6cfdeb77a50a3d71c40dbe24
Eduardo Roque Ildefonso <roqueildefonsoeduardo@gmail.com>
2026-05-27 09:41:02 -0500

    Arreglo definitivo sin gunicorn

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ app = Flask(__name__)
 def inicio():
     return render_template("index.html")
 
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=10000)
+
 
